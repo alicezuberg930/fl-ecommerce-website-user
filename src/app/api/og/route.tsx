7879067843cedc54bfc494dc462815df67058a69
alicezuberg930 <tien23851@gmail.com>
2025-10-10 22:17:03 +0700

    chore: fix bug

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -8,17 +8,19 @@ export function GET(request: NextRequest) {
     const title = searchParams.get('title') ?? ''
     const description = searchParams.get('description') ?? seo.description
 
-    return new ImageResponse((
-        <div
-            tw='flex h-full w-full flex-col p-16 text-white'
-            style={{
-                backgroundColor: '#0c0c0c',
-                backgroundImage: `linear-gradient(to top right, #a96249, transparent)`,
-            }}
-        >
-            <p tw='text-4xl'>{seo.applicationName}</p>
-            <p tw='text-6xl'>{title} </p>
-            <p tw='text-2xl'>{description} </p>
-        </div>
-    ), { width: 1200, height: 630 })
+    return new ImageResponse(
+        (
+            <div tw='flex h-full w-full flex-col p-16 text-white'
+                style={{
+                    backgroundColor: '#0c0c0c',
+                    backgroundImage: `linear-gradient(to top right, #a96249, transparent)`,
+                }}
+            >
+                <p tw='text-4xl'>{seo.applicationName}</p>
+                <p tw='text-6xl'>{title} </p>
+                <p tw='text-2xl'>{description} </p>
+            </div>
+        ),
+        { width: 1200, height: 630 }
+    )
 }
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -9,16 +9,19 @@ import { PATH_API } from "@/routes/paths"
 export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params
     const id = slug.split('-').at(-1)
-    const response = await fetch(`${process.env.BASE_API}${PATH_API.product}/${id}`, {
-        method: "GET", cache: "force-cache", next: { revalidate: 3600 }
-    })
+    const response = await fetch(
+        `${process.env.BASE_API}${PATH_API.product}/${id}`,
+        { method: "GET", cache: "force-cache", next: { revalidate: 3600 } }
+    )
     const result: APIResponse<IProductDetails> = await response.json()
 
     return createSEO({
         title: result.data.name,
         description: result.data.description,
-        image: result.data.images[0],
-        url: `/product/${slug}`,
+        openGraph: {
+            images: result.data.images[0],
+            url: `/product/${slug}`
+        },
     })
 }
 
